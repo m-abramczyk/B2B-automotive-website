8fc5c57ad98a000f70d0f514636e8208f3d63a89
m-abramczyk <maciej.abramczyk@gmail.com>
2025-02-18 01:37:52 +0100

    Finished nav desktop frontend js + layout

--- static/js/base.js
+++ static/js/base.js
@@ -6,10 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('load', () => {
         handleScroll();
+        handleNavBackground();
     });
 
     window.addEventListener('resize', () => {
         handleScroll();
+        handleNavBackground();
     });
 
     ///////////////////////////////////////////////////////////////////
@@ -42,5 +44,113 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
     }
+
+    ///////////////////////////////////////////////////////////////////
+    // Nav Dropdown Content
+
+    const nav = document.querySelector('nav');
+    const dropdowns = nav.querySelectorAll('[data-dropdown]');
+    const dropdownTriggers = nav.querySelectorAll('[data-dropdown-trigger]');
+    const dropdownCloseTriggers = nav.querySelectorAll('[data-dropdown-close-trigger]');
+    const dropdownContent = nav.querySelectorAll('.dropdown-content > li');
+    
+    // Disable tabindex
+    const dropdownContentLinks = nav.querySelectorAll("#nav-desktop .dropdown-content a");
+    dropdownContentLinks.forEach(link => {
+        link.setAttribute('tabindex', '-1');
+    });
+
+    function activateDropdown(trigger) {
+
+        deactivateDropdowns();
+    
+        const currentDropdown = trigger.closest('[data-dropdown]');
+        currentDropdown.classList.add('active');
+
+        // Enable tabindex
+        currentDropdown.querySelectorAll('#nav-desktop .dropdown-content a').forEach(link => {
+            link.setAttribute('tabindex', link.getAttribute('tabindex') === '-1' ? '0' : '-1');
+        });
+        
+    }
+    
+    function deactivateDropdowns() {
+
+        dropdowns.forEach(dropdown => {
+            dropdown.classList.remove('active');
+
+            // Reset Tabindex
+            dropdown.querySelectorAll('#nav-desktop .dropdown-content a').forEach(link => link.setAttribute('tabindex', '-1'));
+        });
+    }
+    
+    // Handle dropdown activation
+    dropdownTriggers.forEach(trigger => {
+        trigger.addEventListener('mouseenter', () => {
+            activateDropdown(trigger);
+            updateTranslationValue();
+        });
+    });
+    
+    // Close dropdowns when leaving nav
+    nav.addEventListener('mouseleave', () => {
+        deactivateDropdowns();
+        updateTranslationValue();
+    });
+    
+    // Close dropdowns when hovering a non-dropdown link
+    dropdownCloseTriggers.forEach(closeTrigger => {
+        closeTrigger.addEventListener('mouseenter', () => {
+            deactivateDropdowns();
+            updateTranslationValue();
+        });
+    });
+
+
+    ///////////////////////////////////////////////////////////////////
+    // Handle Nav Background
+
+    const navBackground = document.querySelector('#nav-background');
+
+    const pageTopObserver = document.createElement('div');
+        pageTopObserver.setAttribute('data-page-top-observer', '');
+        nav.before(pageTopObserver);
+
+    let pageTop = true;
+
+    function handleNavBackground() {
+
+        const navBackgroundObserver = new IntersectionObserver((entries) => {
+            pageTop = entries[0].isIntersecting;
+
+            updateTranslationValue();
+            
+        }, {rootMargin: `20% 0px 0px 0px`,});
+
+        navBackgroundObserver.observe(pageTopObserver);
+    }
+
+    function updateTranslationValue() {
+
+        const navHeight = nav.offsetHeight;
+        const activeDropdown = document.querySelector('#nav-desktop .dropdown.active');
+
+        let translationValue = navHeight;
+
+        if (activeDropdown) {
+            const dropdownContent = activeDropdown.querySelector('.dropdown-content');
+            translationValue = dropdownContent.getBoundingClientRect().bottom;
+        }
+
+        if (!pageTop || nav.matches(':hover')) {
+            navBackground.style.transform = `translateY(${translationValue}px)`;                
+        } else {
+            navBackground.style.transform = 'translateY(0)';
+        }
+
+    }
+
+    nav.addEventListener('mouseenter', updateTranslationValue);
+    nav.addEventListener('mouseleave', updateTranslationValue);
 
 });
